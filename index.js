dc22238d99e388aef3067af3988bb58544058a5a
github username <github email address>
2021-03-23 13:19:01 -0400

    commit tree 1377ce687288f1cf94a278089518c63d77c52601

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+function writeCards(name, event) {                                         let cards = []
+    for (let i = 0; i < name.length; i++) {
+    cards.push( `Thank you, ${name[i]}, for the wonderful ${event} gift!` ); 
+    }
+    return cards 
+
+}
+
+// while ([condition]) {
+//     [loop body]
+// }
+
+
+// const bets = [100, 200, 900, 20000]
+
+// function addBets(bets) {
+//     let i = 0;
+//     while (i < bets.length) {
+//       console.log(`Placed $${bets[i] and added it to betslip`); i ++; 
+
+//     }
+//    return bets;
+// }
+
+// addBets(bets);
+
+
+function countDown(num) {
+    for (let i = 0; num >= 0; num--)
+        console.log(num);
+}
